0c682dee7d55adeb5c6a33300ead1e861ab754b4
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-26 15:50:21 +0300

    adding subject methods

--- flashcard/flash/src/app/subject/show-subject/show-subject.component.ts
+++ flashcard/flash/src/app/subject/show-subject/show-subject.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService} from 'src/app/shared.service';
+
 
 @Component({
   selector: 'app-show-subject',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowSubjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
 
+  GradeList:any=[];
+
+  ModalTitle:string;
+  ActivateAddEditGradeComp:boolean="false";
+  grade:any;
   ngOnInit(): void {
+
+    this.refreshGradeList();
   }
 
+  addClick(){
+    this.grade{
+      GradeId:0,
+      GradeName:""
+
+    }
+
+    this.ModalTitle="Add Grade";
+    this.ActivateAddEditGradeComp=true;
+  }
+
+  // editclick
+  editClick(item){
+    this.grade = item;
+    this.ModalTitle='Edit Grade';
+    this.ActivateAddEditGrade=true;
+  }
+
+  // delete
+  deleteClick(item){
+    if(confirm('Are you sure??')){
+      this.service.deleteGrade(item.GradeId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshGradeList();
+      })
+    }
+  }
+
+  // close click
+  closeClick(){
+    this.ActivateAddEditGradeComp=false;
+    this.refreshGradeList();
+  }
+
+  refreshGradeList(){
+    this.service.getGradeList().subscribe(data=>{
+      this.GradeList=data;
+    });
+  }
 }
